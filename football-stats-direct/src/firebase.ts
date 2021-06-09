ed8af0a31147a004857421a8db407b1e8cbfc3b5
Viqas Hussain <viqashussain@hotmail.co.uk>
2021-06-09 10:58:58 +0100

    Update firebase.ts

--- football-stats-direct/src/firebase.ts
+++ football-stats-direct/src/firebase.ts
@@ -1,14 +1,5 @@
 import firebase from "firebase";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyApOzn81BSTDzWblbAqnFAdSa3-M-fDhUc",
-    authDomain: "footballstatsdirect.firebaseapp.com",
-    projectId: "footballstatsdirect",
-    storageBucket: "footballstatsdirect.appspot.com",
-    messagingSenderId: "239765740732",
-    appId: "1:239765740732:web:366a0007b1394cc84b44b9",
-    measurementId: "G-WH3TQQ5RMB"
-  };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
